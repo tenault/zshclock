10cd4459f156a48804563b307e0d55e95e3c6e64
Malakai Smith <hello@malakai.net>
2025-07-22 17:00:15 -0600

    reimplement command bar

--- zshclock.zsh
+++ zshclock.zsh
@@ -1,96 +1,21 @@
 #!/usr/bin/env zsh
 
-# todo
-# save live settings to config file
-# auto-size down to single date line
-# add alarm mode
-# tab cycles modes?
-# tab in cmdr autocompletes command
-# register custom components?
-# support non-fullscreen mode
-# zc_paint: auto-sizing from content, auto-position alignments, scrolling, wrapping, content alignments, and z-index
-# error handling (use -q|--quiet for disabling error messages)
-# background mode? pause repaints? (disables input and only updates every minute)
-# custom expansions for bold, italic, underline, colors, etc
-
-###### functions
-
-### clock interaction
-
-#function enter_cmd_mode {
-#    clock[mode:cmd]=1
+#                    dP                dP                   dP
+#                    88                88                   88
+#  d888888b .d8888b. 88d888b. .d8888b. 88 .d8888b. .d8888b. 88  .dP
+#     .d8P' Y8ooooo. 88'  `88 88'  `"" 88 88'  `88 88'  `"" 88888"
+#   .Y8P          88 88    88 88.  ... 88 88.  .88 88.  ... 88  `8b.
+#  d888888P `88888P' dP    dP `88888P' dP `88888P' `88888P' dP   `YP
+# ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
 #
-#    print -n $PC_CURSOR_SHOW
+# copyright (c) 2025 Malakai Smith (@tenault)
+# originally forked from @octobanana/peaclock
 #
-#    refresh_cmdr
-#}
-
-#function exit_cmd_mode {
-#    clock[mode:cmd]=0
-#    clock[cmd]=""
+# ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
 #
-#    print -n $PC_CURSOR_HIDE
-#
-#    zcurses clear cmdr
-#    zcurses refresh cmdr
-#}
-
-#function get_input {
-#    local keypress
-#    local special
-#
-#    zcurses input cmdr keypress special
-#
-#    if [[ -n $special ]]; then
-#        case $special in
-#            (BACKSPACE) if (( $clock[mode:cmd] )); then clock[cmd]=${clock[cmd]%?}; refresh_cmdr; fi ;;
-#            (*)         ;;
-#        esac
-#    elif (( ! $clock[mode:cmd] )); then
-#        case $keypress in
-#            (q|Q) break ;;
-#            (:)   enter_cmd_mode ;;
-#            (*)   ;;
-#        esac
-#    else
-#        case $keypress in
-#            ('')          ;;
-#            ($'\e')       exit_cmd_mode ;;
-#            ($'\n'|$'\r') exit_cmd_mode ;; # process $clock[cmd]
-#            ($'\t')       clock[cmd]+="    "; refresh_cmdr    ;;
-#            (*)           clock[cmd]+=$keypress; refresh_cmdr ;;
-#        esac
-#    fi
-#}
-
-### clock components
-
-#function build_cmdr {
-#    components+=(cmdr)
-#
-#    clock[cmdr:h]=1
-#    clock[cmdr:w]=$clock[vw]
-#    clock[cmdr:y]=$clock[vh]
-#    clock[cmdr:x]=0
-#
-#    zcurses addwin cmdr $clock[cmdr:h] $clock[cmdr:w] $clock[cmdr:y] $clock[cmdr:x]
-#    zcurses timeout cmdr $g_rate_input
-#
-#    if (( clock[mode:cmd] )); then refresh_cmdr; fi
-#}
-
-#function refresh_cmdr {
-#    local cmd=":$clock[cmd]"
-#
-#    if (( ${#clock[cmd]} >= clock[cmdr:w] )); then cmd=":...${clock[cmd]:$(( -clock[cmdr:w] + 4 ))}"; fi
-#
-#    zcurses clear cmdr
-#    zcurses move cmdr 0 0
-#    zcurses string cmdr $cmd
-#    zcurses refresh cmdr
-#}
-
-###### EVERYTHING ABOVE THIS LINE SUBJECT TO DELETION ######
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at https://mozilla.org/MPL/2.0
 
 
 ###### constants
@@ -124,8 +49,8 @@ function zc_build {
 }
 
 function zc_drive {
-    local _epoch=$EPOCHREALTIME
-    local _epsilon=0
+    float _epoch=$EPOCHREALTIME
+    integer _epsilon=0
 
     while true; do
         # handle inputs
@@ -135,7 +60,7 @@ function zc_drive {
         zc_align
 
         # repaint clock
-        local _duration=${$(( (EPOCHREALTIME - _epoch) * 1000 ))%%.*}
+        integer _duration=${$(( (EPOCHREALTIME - _epoch) * 1000 ))%%.*}
 
         if (( _duration >= ( zc[:rate:refresh] - _epsilon ) )); then
             zc_cycle
@@ -147,9 +72,21 @@ function zc_drive {
 }
 
 function zc_clean {
-    local _code=${1:-0}
+    integer _code=${1:-0}
     zc_write $ZC_CURSOR_SHOW $ZC_EXIT
     exit $_code
+}
+
+
+###### plonk
+
+function zc_plonk {
+    zc[:date]="%a %b %d - %r"
+    zc[:rate:input]=50
+    zc[:rate:refresh]=1000
+
+    local _components=(date commander)
+    zc_pack components _components
 }
 
 
@@ -163,48 +100,54 @@ function zc_align {
 }
 
 function zc_cycle {
-    local _components
-    zc_unpack components _components
+    local _components=$@
+    if (( $# == 0 )); then zc_unpack components _components; fi
 
     for name in $_components; do "zc_set_$name"; done
 
-    zc_paint
-}
-
-
-###### plonkers
-
-function zc_plonk {
-    zc[:date]="%a %b %d - %r"
-    zc[:rate:input]=50
-    zc[:rate:refresh]=2000
-
-    local _components=(date)
-    zc_pack components _components
+    zc_paint $1
 }
 
 
 ###### engines
 
 function zc_paint {
-    local _h
-    local _w
-    local _y
-    local _x
+    integer _y
+    integer _x
+    integer _h
+    integer _w
 
     local _data
 
-    local _origin
     local _staged=()
+    local _clear=$ZC_CLEAR
+    local _origin
 
-    local _components
-    zc_unpack components _components
+    local _components=$@
+
+    if (( $# == 0 )); then
+        zc_unpack components _components
+    else
+        _clear=$ZC_CLEAR_LINE
+    fi
 
     for name in $_components; do
         _h=$zc[$name:h]
-        _w=$zc[$name:w]
-        _y=$zc[$name:y]
-        _x=$zc[$name:x]
+
+        case $zc[$name:w] in
+            (:auto) _w=${#zc[$name:data]} ;;
+            (*)     _w=$zc[$name:w]       ;;
+        esac
+
+        case $zc[$name:y] in
+            (:auto) _y=$(( ( (zc[vh] - _h) / 2 ) + zc[vh] % 2 )) ;;
+            (*)     _y=$zc[$name:y] ;;
+        esac
+
+        case $zc[$name:x] in
+            (:auto) _x=$(( ( (zc[vw] - _w) / 2 ) + zc[vw] % 2 )) ;;
+            (*)     _x=$zc[$name:x] ;;
+        esac
 
         _origin="${ZC_CSI}${_y};${_x}H"
         _data=$zc[$name:data]
@@ -212,17 +155,41 @@ function zc_paint {
         _staged+=($_origin $_data)
     done
 
-    zc_write $ZC_CLEAR ${(j::)_staged} $2
+
+    zc_write $_clear ${(j::)_staged}
 }
 
 function zc_input {
     local _key
-    read -rs -t $(( zc[:rate:input] / 1000.0 )) -k 1 _key
+    read -s -t $(( zc[:rate:input] / 1000.0 )) -k 1 _key
 
-    case $_key in
-        (q|Q) break ;;
-        (*)   ;;
-    esac
+    if (( zc[commander] )); then
+        case $_key in
+            ($'\e')
+                zc[:command]=""
+                zc[commander]=0
+                ;;
+            ($'\b'|$'\x7f')
+                zc[:command]=${zc[:command]%?}
+                ;;
+            ($'\n'|$'\r')
+                # parse command
+                zc[:command]=""
+                zc[commander]=0
+                ;;
+            (*)
+                zc[:command]+=$_key
+                ;;
+        esac
+
+        zc_cycle commander
+    else
+        case $_key in
+            (q|Q) break ;;
+            (:)   zc[commander]=1; zc_cycle commander ;;
+            (*)   ;;
+        esac
+    fi
 }
 
 function zc_parse {
@@ -233,23 +200,42 @@ function zc_parse {
 ###### components
 
 function zc_add_date {
-    local _date
-    strftime -s _date $zc[:date]
-
+    zc[date:y]=:auto
+    zc[date:x]=:auto
     zc[date:h]=1
-    zc[date:w]=${#_date}
-    zc[date:y]=$(( ((zc[vh] - zc[date:h]) / 2) + zc[vh] % 2 ))
-    zc[date:x]=$(( ((zc[vw] - zc[date:w]) / 2) + zc[vw] % 2 ))
+    zc[date:w]=:auto
 
-    zc[date:data]=$_date
+    zc_set_date
 }
 
 function zc_set_date {
     local _date
     strftime -s _date $zc[:date]
+
     zc[date:data]=$_date
 }
 
+function zc_add_commander {
+    zc[:command]=${zc[:command]:-}
+
+    zc[commander:y]=$zc[vh]
+    zc[commander:x]=0
+    zc[commander:h]=1
+    zc[commander:w]=$zc[vw]
+
+    zc[commander]=${zc[commander]:-0}
+
+    zc_set_commander
+}
+
+function zc_set_commander {
+    local _command=$zc[:command]
+
+    # truncate overflows
+    if (( ${#_command} >= zc[commander:w] )); then _command=...${_command:$(( -zc[commander:w] + 4 ))}; fi
+
+    if (( zc[commander] )); then zc[commander:data]=:${_command//\\/\\\\}$ZC_CURSOR_SHOW; else zc[commander:data]=$ZC_CURSOR_HIDE; fi
+}
 
 ###### helpers
 
@@ -272,7 +258,7 @@ function zsh_that_clock {
     zc_plonk
 
     # init
-    zc_write $ZC_INIT $ZC_CURSOR_HIDE
+    zc_write $ZC_INIT
 
     # ztc
     zc_build && zc_drive
